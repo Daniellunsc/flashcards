c0af7a0528247ccd6191dc3a82a22dc2ca9abac3
Daniel Luna <daniel@macbook-daniel.local>
2018-04-21 12:32:13 -0300

    Testes com a API

--- App.js
+++ App.js
@@ -5,7 +5,19 @@ import * as API from './helpers/api';
 export default class App extends React.Component {
 
   componentDidMount() {
-
+    API.initDeck()
+      .then(() => { return API.getDecks() })
+      .then(() => { return API.saveDeckTitle('Probono') })
+      .then(() => { return API.getDecks() })
+      .then(res => console.log(JSON.parse(res)))
+      .then(() => { return API.getDeck('Probono') })
+      .then(res => console.log(res))
+      .then(() => API.addCardToDeck('Probono', { question: 'who?', answer: 'me' }))
+      .then(() => { return API.getDecks() })
+      .then(res => console.log(JSON.parse(res)))
+      .then(() => API.addCardToDeck('JavaScript', { question: 'who?2', answer: 'me2' }))
+      .then(() => { return API.getDecks() })
+      .then(res => console.log(JSON.parse(res)))
   }
 
   render() {
